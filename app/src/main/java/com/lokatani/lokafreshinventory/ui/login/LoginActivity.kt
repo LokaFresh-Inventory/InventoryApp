ab644d6a03e4749f4fe952f64294aef879628989
Buzank <buzankkun@gmail.com>
2025-03-25 23:13:13 +0700

    style(login): optimizing the code for login UI

--- app/src/main/java/com/lokatani/lokafreshinventory/ui/login/LoginActivity.kt
+++ app/src/main/java/com/lokatani/lokafreshinventory/ui/login/LoginActivity.kt
@@ -1,32 +1,33 @@
 package com.lokatani.lokafreshinventory.ui.login
 
 import android.os.Bundle
-import android.text.InputType
 import androidx.appcompat.app.AppCompatActivity
 import com.lokatani.lokafreshinventory.R
 import com.lokatani.lokafreshinventory.databinding.ActivityLoginBinding
+import android.util.Patterns
+import androidx.core.widget.addTextChangedListener
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
-    private var isPasswordVisible = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.showPasswordButton.setOnClickListener {
-            isPasswordVisible = !isPasswordVisible
-            if (isPasswordVisible) {
-                binding.etPassword.inputType =
-                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-                binding.showPasswordButton.setImageResource(R.drawable.eye)
-            } else {
-                binding.etPassword.inputType =
-                    InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-                binding.showPasswordButton.setImageResource(R.drawable.eye_hidden)
+
+        binding.apply {
+            edEmail.addTextChangedListener {
+                var email = edEmail.text.toString()
+                    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                        tilEmail.isErrorEnabled = true
+                        tilEmail.error = "Email invalid"
+                    } else {
+                        tilEmail.error = null
+                        tilEmail.isErrorEnabled = false
+                    }
             }
-            binding.etPassword.setSelection(binding.etPassword.text.length)
+
         }
     }
 }
