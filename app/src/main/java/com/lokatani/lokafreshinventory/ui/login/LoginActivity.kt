6ec7625214ec46022dc5f3fbbdb4ed88413808fe
Buzank <buzankkun@gmail.com>
2025-03-29 00:37:10 +0700

    refactor(project): reformat and rearrange code

--- app/src/main/java/com/lokatani/lokafreshinventory/ui/login/LoginActivity.kt
+++ app/src/main/java/com/lokatani/lokafreshinventory/ui/login/LoginActivity.kt
@@ -2,12 +2,13 @@ package com.lokatani.lokafreshinventory.ui.login
 
 import android.content.Intent
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.lokatani.lokafreshinventory.databinding.ActivityLoginBinding
 import android.util.Patterns
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.addTextChangedListener
 import com.lokatani.lokafreshinventory.MainActivity
+import com.lokatani.lokafreshinventory.databinding.ActivityLoginBinding
 import com.lokatani.lokafreshinventory.ui.register.RegisterActivity
+
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
 
@@ -20,13 +21,13 @@ class LoginActivity : AppCompatActivity() {
         binding.apply {
             edEmail.addTextChangedListener {
                 var email = edEmail.text.toString()
-                    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
-                        tilEmail.isErrorEnabled = true
-                        tilEmail.error = "Email invalid"
-                    } else {
-                        tilEmail.error = null
-                        tilEmail.isErrorEnabled = false
-                    }
+                if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+                    tilEmail.isErrorEnabled = true
+                    tilEmail.error = "Email invalid"
+                } else {
+                    tilEmail.error = null
+                    tilEmail.isErrorEnabled = false
+                }
             }
 
             btnLogin.setOnClickListener {
