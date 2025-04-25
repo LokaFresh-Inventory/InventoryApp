bf8949429557e774de6b855bafb03c3fd5a76118
Buzank <buzankkun@gmail.com>
2025-04-25 15:54:10 +0700

    feat(app): change app's Icon and add Splash Screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -77,6 +77,7 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.activity)
+    implementation(libs.androidx.core.splashscreen)
 
     // Testing
     testImplementation(libs.junit)
